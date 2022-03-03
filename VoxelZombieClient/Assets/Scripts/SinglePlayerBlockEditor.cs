63faabb7383929e69aa958ae2524ebb78eb58c24
Russell Ruffolo <ruffolorussell@gmail.com>
2022-03-03 16:20:48 -0500

    Started adding SinglePlayer mode to client

---
+++ VoxelZombieClient/Assets/Scripts/SinglePlayerBlockEditor.cs
@@ -0,0 +1,17 @@
+﻿namespace Client
+{
+    public class SinglePlayerBlockEditor : BaseBlockEditor
+    {
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnBreakBlock(ushort x, ushort y, ushort z)
+        {
+        }
+
+        protected override void OnPlaceBlock(ushort x, ushort y, ushort z, ushort blockTag)
+        {
+        }
+    }
+}
