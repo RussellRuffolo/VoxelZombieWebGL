95c1cbf634ba2c7aab071697d37564da3aa63c67
Snapper <learningwithsnapper@gmail.com>
2022-03-04 14:31:50 -0500

    Add Docker

--- crashblox/server.js
+++ crashblox/server.js
@@ -1,19 +1,33 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-var path = require('path')
+const helmet = require('helmet')
+const path = require('path');
 
-const hostname = '192.168.0.171';
+const app = express();
+
+const hostname = '0.0.0.0';
 const port = 80
 
 app.use(cors());
+// app.use(helmet())
+// //Todo: Use nonce
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             scriptSrc: ["'self'", "'unsafe-inline'", 'blob:http://localhost:8000/']
+//         }
+//     })
+// )
+// app.use(
+//     helmet({
+//         contentSecurityPolicy: false
+//     })
+// )
 app.use(express.static(path.join(__dirname, 'Public')));
-
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     next()
-  })
-  
+})
 
-app.listen(port, hostname, () => console.log('WebGL Server is listening on port: ' + port));
+app.listen(port, hostname, () => console.log('WebGL Server is listening on ' + hostname + ':' + port));
