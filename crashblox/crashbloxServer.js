cf7272b89f108fd9576ec663b6496c78e2b756f4
Russell Ruffolo <ruffolorussell@gmail.com>
2022-04-14 11:37:39 -0400

    html test

--- crashblox/crashbloxServer.js
+++ crashblox/crashbloxServer.js
@@ -6,8 +6,11 @@ var path = require('path')
 const configData = require('./config.json');
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'Public')));
+//app.use(express.static(path.join(__dirname, 'Public')));
 
+app.get('/', (req, res) => {
+    res.render('index')
+  })
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
