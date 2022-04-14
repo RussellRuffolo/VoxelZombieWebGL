0b9d431c85a757b97067c224aca506cf2c9b2c68
Russell Ruffolo <ruffolorussell@gmail.com>
2022-04-14 09:08:52 -0700

    static

--- crashblox/crashbloxServer.js
+++ crashblox/crashbloxServer.js
@@ -6,10 +6,31 @@ var path = require('path')
 const configData = require('./config.json');
 
 app.use(cors());
-//app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'Public/Static')));
+
+app.use(express.static(path.join(__dirname, 'Public/TemplateData')));
+
+app.use(express.static(path.join(__dirname, 'Public/Build')));
+app.use(express.static(path.join(__dirname, 'Public/StreamingAssets')));
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.sendFile(path.join(__dirname, 'Public/index.html'))
+  })
+
+  app.get('/landing.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Public/landing.html'))
+  })
+
+  app.get('/blog.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Public/blog.html'))
+  })
+
+  app.get('/developers.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Public/developers.html'))
+  })
+
+  app.get('/game.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Public/game.html'))
   })
 
 app.use(function (req, res, next) {
