2de1a3b5088f524f675148f3da17ce9218ba738b
Snapper <learningwithsnapper@gmail.com>
2022-04-20 00:08:50 -0400

    Pythonic open statement

--- identity/main.py
+++ identity/main.py
@@ -1,13 +1,14 @@
 import uvicorn
+import json
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-import json
-f = open('../crashblox/config.json')
-
-data = json.load(f)
+# Todo: This code works, but we should really merge .env and config.json
+with open("../crashblox/config.json") as f:
+    data = json.load(f)
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host=data['hostAddress'], log_level="info")
+    uvicorn.run("app.app:app", host=data["hostAddress"], log_level="info")
