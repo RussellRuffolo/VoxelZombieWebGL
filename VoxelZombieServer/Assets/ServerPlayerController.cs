5402b18730993872bb19365babe33c353ad432d7
Russell Ruffolo <ruffolorussell@gmail.com>
2022-10-27 20:11:30 -0600

    Server Build

--- VoxelZombieServer/Assets/ServerPlayerController.cs
+++ VoxelZombieServer/Assets/ServerPlayerController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class ServerPlayerController : MonoBehaviour
