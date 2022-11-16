cfe1b87cbc84f756335f9b4acc11994e5dd98c60
Russell Ruffolo <ruffolorussell@gmail.com>
2022-11-16 13:48:44 -0700

    Added model file and updated client chunking

--- VoxelZombieClient/Assets/Scripts/ClientChunk.cs
+++ VoxelZombieClient/Assets/Scripts/ClientChunk.cs
@@ -105,45 +105,44 @@ public class ClientChunk : Chunk
         {
             voxels[i] = reader.ReadByte();
         }
-
-        Debug.Log("PROCESSED CHUNK CHANGE");
+        
         dirty = true;
-        //
-        // int vertexCount = reader.ReadInt();
-        //
-        // for (int i = 0; i < vertexCount; i++)
-        // {
-        //     vertices.Add(new Vector3(reader.ReadFloat(), reader.ReadFloat(), reader.ReadFloat()));
-        // }
-        //
-        // for (int i = 0; i < 55; i++)
-        // {
-        //     int triangleCount = reader.ReadInt();
-        //
-        //     for (int j = 0; j < triangleCount; j++)
-        //     {
-        //         TriangleLists[i].Add(reader.ReadInt());
-        //     }
-        // }
-        //
-        // mesh.SetVertices(vertices);
-        //
-        // for (int i = 0; i < 55; i++)
-        // {
-        //     mesh.SetTriangles(TriangleLists[i].ToArray(), i);
-        // }
-        //
-        // for (int i = 0; i < vertexCount; i++)
-        // {
-        //     normals.Add(Vector3.up);
-        // }
-        //
-        //
-        // mesh.SetNormals(normals);
-        // mesh.SetUVs(0, GreedyChunk.UvCalculator.CalculateUVs(vertices.ToArray(), 1).ToList());
-        //
-        //
-        // meshFilter.mesh = mesh;
-        // meshCollider.sharedMesh = mesh;
+        
+        int vertexCount = reader.ReadInt();
+        
+        for (int i = 0; i < vertexCount; i++)
+        {
+            vertices.Add(new Vector3(reader.ReadFloat(), reader.ReadFloat(), reader.ReadFloat()));
+        }
+        
+        for (int i = 0; i < 55; i++)
+        {
+            int triangleCount = reader.ReadInt();
+        
+            for (int j = 0; j < triangleCount; j++)
+            {
+                TriangleLists[i].Add(reader.ReadInt());
+            }
+        }
+        
+        mesh.SetVertices(vertices);
+        
+        for (int i = 0; i < 55; i++)
+        {
+            mesh.SetTriangles(TriangleLists[i].ToArray(), i);
+        }
+        
+        for (int i = 0; i < vertexCount; i++)
+        {
+            normals.Add(Vector3.up);
+        }
+        
+        
+        mesh.SetNormals(normals);
+        mesh.SetUVs(0, GreedyChunk.UvCalculator.CalculateUVs(vertices.ToArray(), 1).ToList());
+        
+        
+        meshFilter.mesh = mesh;
+        meshCollider.sharedMesh = mesh;
     }
 }
