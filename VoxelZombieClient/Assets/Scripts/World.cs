f9bb0003960d4a9da1da2fbaa18e86fe3582d372
Russell Ruffolo <ruffolorussell@gmail.com>
2022-11-17 14:35:30 -0700

    Improved logic

--- VoxelZombieClient/Assets/Scripts/World.cs
+++ VoxelZombieClient/Assets/Scripts/World.cs
@@ -16,7 +16,7 @@ public class World : IWorld
 
     public bool IsInChunkBounds(ChunkID id)
     {
-        return id.Z < VoxelEngine.Width / 8 && id.X < VoxelEngine.Length / 8 && id.Y < VoxelEngine.Height / 8 && id.Z > 0 && id.Y > 0 && id.Z > 0;
+        return id.Z < VoxelEngine.Width / 8 && id.X < VoxelEngine.Length / 8 && id.Y < VoxelEngine.Height / 8 && id.Z >= 0 && id.Y >= 0 && id.Z >= 0;
     }
 
     public IVoxelEngine VoxelEngine { get; set; }
