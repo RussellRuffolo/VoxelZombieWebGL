a775b6be44a049d43b78864cc03751c19dfe26a3
Snapper Vibes <learningwithsnapper@gmail.com>
2022-11-22 13:33:30 -0500

    Refactored project's skeleton

---
+++ VoxelZombie/Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Tests;
+using ZombieLib;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ VoxelZombie/ZombieLib/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ZombieLib
+{
+    public class Class1
+    {
+
+    }
+}
