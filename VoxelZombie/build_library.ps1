70cfa13187d7ef20b66a58277390bd116fd3bfe7
Snapper Vibes <learningwithsnapper@gmail.com>
2022-11-22 15:34:40 -0500

    Script now also coppies to server

--- VoxelZombie/build_library.ps1
+++ VoxelZombie/build_library.ps1
@@ -1,6 +1,18 @@
 ﻿dotnet clean "ZombieLib\" 
-dotnet build "ZombieLib\" 
+dotnet build "ZombieLib\"
+
+echo $("-" * 50)
+
+
 $dependencies = "Client\Assets\Dependencies\"
+echo "Copying debug build to Client"
 if (!(Test-Path $dependencies) ){mkdir $dependencies}
 cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.dll"  $dependencies
 cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.pdb" $dependencies
+
+$dependencies = "Server\Assets\Dependencies\"
+echo "Copying debug build to Server"
+if (!(Test-Path $dependencies) ){mkdir $dependencies}
+cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.dll"  $dependencies
+cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.pdb" $dependencies
+
