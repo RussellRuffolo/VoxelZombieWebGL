4bf6f7f25b895c25f232854fe3e4e354a2edf349
Snapper Vibes <learningwithsnapper@gmail.com>
2022-11-23 10:49:21 -0500

    Build copies to both Client and Server

--- VoxelZombie/build_library.ps1
+++ VoxelZombie/build_library.ps1
@@ -1,6 +1,16 @@
 ﻿dotnet clean "ZombieLib\" 
-dotnet build "ZombieLib\" 
-$dependencies = "Client\Assets\Dependencies\"
-if (!(Test-Path $dependencies) ){mkdir $dependencies}
-cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.dll"  $dependencies
-cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.pdb" $dependencies
+dotnet build "ZombieLib\"
+
+echo $("-"*50)
+
+$client_dependencies = "Client\Assets\Dependencies\"
+if (!(Test-Path $client_dependencies) ){mkdir $client_dependencies}
+cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.dll"  $client_dependencies
+cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.pdb" $client_dependencies
+echo "Copied ZombieLib debug build to $client_dependencies"
+
+$server_dependencies = "Server\Assets\Dependencies\"
+if (!(Test-Path $server_dependencies) ){mkdir $server_dependencies}
+cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.dll"  $server_dependencies
+cp "ZombieLib\bin\Debug\netstandard2.0\ZombieLib.pdb" $server_dependencies
+echo "Copied ZombieLib debug build to $server_dependencies"
