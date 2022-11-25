5d054343ee2b7a31244c37c3b1a39b0dbdcb5246
Snapper Vibes <learningwithsnapper@gmail.com>
2022-11-25 13:24:48 -0500

    .

--- VoxelZombie/Tests/UnitTest1.cs
+++ VoxelZombie/Tests/UnitTest1.cs
@@ -16,19 +16,4 @@ public class Tests
         Assert.That((byte)0, Is.EqualTo((byte)Voxel.Air));
     }
 
-    [Test]
-    public void TestCastVoxelsAsBytes()
-    {
-        byte[] bytes = { 0, 3, 7};
-        Voxel[] voxels = { Voxel.Air, Voxel.Dirt, Voxel.Bedrock };
-
-        byte[] casted = Cast.AsBytes(ref voxels);
-        bytes.Zip(casted, (b, v) =>
-        {
-            Assert.That(b, Is.EqualTo(v));
-            return true;  // Todo (Russell): Make idiomatic
-        });
-        // Assert.That(retyped, Is.EqualTo(bytes));
-        // Assert.That(Unsafe.As<Voxel[], byte[]>(ref voxels), Is.EquivalentTo(bytes));
-    }
 }
